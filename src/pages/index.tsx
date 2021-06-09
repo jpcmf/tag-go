42d556de76258a1db5a3510211c13db7e0a9fc19
João Paulo Fricks <jpfricks@gmail.com>
2021-06-08 22:13:28 -0300

    feat: create the header component 🍩

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Button, Flex, Stack, Text } from '@chakra-ui/react';
 
 import { Input } from '../components/Form/Input';
 
@@ -15,6 +15,29 @@ export default function SignIn() {
         flexDir="column"
       >
         <Stack spacing={4}>
+          <Text
+            fontSize="3xl"
+            fontWeight="bold"
+            letterSpacing="tighter"
+            w="64"
+            align="center"
+            margin="auto"
+            h="12"
+          >
+            TAG Go
+            <Text color="messenger.400" as="span" ml="1">
+              .
+            </Text>
+            <Text
+              as="small"
+              color="gray.700"
+              ml="1"
+              fontSize="smaller"
+              fontWeight="300"
+            >
+              dashboard
+            </Text>
+          </Text>
           <Input name="email" id="email" type="email" label="E-mail" />
           <Input name="password" id="password" type="password" label="Senha" />
         </Stack>
