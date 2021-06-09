14621a8cd04c74608e6481039660e23d2960b7ba
João Paulo Fricks <jpfricks@gmail.com>
2021-06-08 23:29:33 -0300

    feat: create the sidebar component 🍩

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,64 @@
+import { Box, Stack, Text, Link, Icon, Flex } from '@chakra-ui/react';
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from 'react-icons/ri';
+
+export function Sidebar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text
+            textTransform="uppercase"
+            fontWeight="bold"
+            color="gray.400"
+            fontSize="small"
+          >
+            Geral
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center">
+              <Icon as={RiDashboardLine} fontSize="20" color="blue.400" />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" align="center">
+              <Icon as={RiContactsLine} fontSize="20" color="blue.400" />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+        <Box>
+          <Text
+            textTransform="uppercase"
+            fontWeight="bold"
+            color="gray.400"
+            fontSize="small"
+          >
+            Automação
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center">
+              <Icon as={RiInputMethodLine} fontSize="20" color="blue.400" />
+              <Text ml="4" fontWeight="medium">
+                Formulários
+              </Text>
+            </Link>
+            <Link display="flex" align="center">
+              <Icon as={RiGitMergeLine} fontSize="20" color="blue.400" />
+              <Text ml="4" fontWeight="medium">
+                Automação
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
