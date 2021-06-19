5bcc32bfc0b033437ec3e99f850e86636d54c777
João Paulo <musicjoaopaulo@gmail.com>
2021-06-19 20:50:31 -0300

    feat: add the behavior of sidebar using context and chakra-ui hooks 🍩

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
+
 import { Logo } from './Logo';
 import { Notification } from './Notification';
 import { Profile } from './Profile';
 import { Search } from './Search';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isVisible = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +26,17 @@ export function Header() {
       align="center"
       px="6"
     >
+      {!isVisible && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          font-size="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
+
       <Logo showLogoData={isVisible} />
 
       {isVisible && <Search />}
