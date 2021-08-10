8bf861f60c24db8ee4c43346c6f79b4854e1a65a
João Paulo <musicjoaopaulo@gmail.com>
2021-08-09 23:32:08 -0300

    feat: add react-hook-form to handle forms, configure login form, add loading ⚙️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import { Button, Flex, Stack, Text } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -13,6 +28,7 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
           <Text
@@ -38,11 +54,29 @@ export default function SignIn() {
               dashboard
             </Text>
           </Text>
-          <Input name="email" id="email" type="email" label="E-mail" />
-          <Input name="password" id="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            id="email"
+            type="email"
+            label="E-mail"
+            {...register('email', { required: 'E-mail obrigatório' })}
+          />
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            label="Senha"
+            {...register('password', { required: 'Password obrigatório' })}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="messenger" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="messenger"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
