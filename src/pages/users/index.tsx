c8a1dd50e55d1686b39d49aff454eebc40849813
João Paulo <musicjoaopaulo@gmail.com>
2021-09-13 13:14:57 -0300

    feat: create the pagination logic ⚙️

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import {
   Box,
@@ -23,7 +24,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -86,7 +88,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px="{['4','4','6']}">
@@ -123,9 +125,9 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCount={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCount={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
