ef6799a9263770082eda83e12a3093886ff0e7a1
João Paulo Fricks <jpfricks@gmail.com>
2023-09-30 19:21:29 -0300

    feat: Add and configure `next-auth` to handle with authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,15 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+
 import { makeServer } from '../services/mirage';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient } from '../services/queryClient';
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   makeServer();
+// }
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
